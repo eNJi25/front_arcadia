270339812b333205dc91dc8a9b429a5502ec8a47
eNJi25 <nfischer.contact@gmail.com>
2024-11-10 14:18:41 +0100

    Ajout de la page signin.html + mise en place arborescence js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,7 +7,8 @@ export const allRoutes = [
   new Route("/habitats", "Habitats", "/pages/habitats.html"),
   new Route("/contact", "Contact", "/pages/contact.html"),
   new Route("/avis", "Avis", "/pages/avis.html"),
-  new Route("/connect", "Connexion", "/pages/connect.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/editPassword", "Mot de passe oublié", "/pages/auth/editPassword.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
