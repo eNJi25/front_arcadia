3943a8fb52d879b5c17df85ff8381a8f5f73354d
eNJi25 <nfischer.contact@gmail.com>
2024-11-13 14:24:30 +0100

    Ajout modal détail animal sur clque bouton ds page habitat

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,9 +6,13 @@ export const allRoutes = [
   new Route("/services", "Services", "/pages/services.html"),
   new Route("/habitats", "Habitats", "/pages/habitats.html"),
   new Route("/contact", "Contact", "/pages/contact.html"),
-  new Route("/avis", "Avis", "/pages/avis.html"),
+  new Route("/tableau", "Tableau de bord", "/pages/tableau.html"),
   new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-  new Route("/editPassword", "Mot de passe oublié", "/pages/auth/editPassword.html"),
+  new Route(
+    "/editPassword",
+    "Mot de passe oublié",
+    "/pages/auth/editPassword.html"
+  ),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
