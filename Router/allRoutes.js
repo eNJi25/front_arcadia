a98148aa63a065c2b48ca5463b1a611614f573a4
eNJi25 <nfischer.contact@gmail.com>
2024-11-13 17:56:32 +0100

    préparation partie interactif avec js page signin

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,7 +7,12 @@ export const allRoutes = [
   new Route("/habitats", "Habitats", "/pages/habitats.html"),
   new Route("/contact", "Contact", "/pages/contact.html"),
   new Route("/tableau", "Tableau de bord", "/pages/tableau.html"),
-  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route(
+    "/signin",
+    "Connexion",
+    "/pages/auth/signin.html",
+    "/assets/js/auth/signin.js"
+  ),
   new Route(
     "/editPassword",
     "Mot de passe oublié",
