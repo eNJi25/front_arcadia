90bcffad532b9acd70d3a825ff1970ea472d9da5
eNJi25 <nfischer.contact@gmail.com>
2024-11-19 18:00:23 +0100

    Fecth pour afficher les habitats et les animaux sur la page habitat

--- assets/js/habitat.js
+++ assets/js/habitat.js
@@ -1,3 +1,72 @@
+// Fetch affichage
+// Appel pour récupérer tous les habitats
+fetch("https://127.0.0.1:8000/api/habitat/showAll")
+  .then((response) => response.json())
+  .then((habitats) => {
+    const habitatsContainer = document.getElementById("habitats-container");
+    habitats.forEach((habitat) => {
+      // Créer la section de l'habitat
+      const habitatSection = document.createElement("section");
+      habitatSection.id = habitat.nom.toLowerCase();
+
+      habitatSection.innerHTML = `
+                    <div class="container-fluid p-0">
+                        <h2 class="text-center text-secondary mb-3">${habitat.nom}</h2>
+                        <p class="text-primary text-center">${habitat.description}</p>
+                        <div class="row m-0">
+                            <img class="img-habitat" src="${habitat.image}" alt="${habitat.nom}">
+                        </div>
+                        <h3 class="text-center text-secondary my-3">Animaux de la ${habitat.nom}</h3>
+                        <div class="row m-2 g-3" id="animals-${habitat.id}">
+                        </div>
+                        <div class="d-flex justify-content-center my-3">
+                            <a href="#" class="btn btn-secondary">Tous les animaux de la ${habitat.nom}</a>
+                        </div>
+                    </div>
+                `;
+
+      habitatsContainer.appendChild(habitatSection);
+
+      // Appel pour récupérer les 4 derniers animaux de l'habitat
+      fetch(`https://127.0.0.1:8000/api/animal/showlastAnimals/${habitat.id}`)
+        .then((response) => response.json())
+        .then((animals) => {
+          const animalsContainer = document.getElementById(
+            `animals-${habitat.id}`
+          );
+          animals.forEach((animal) => {
+            // Vérifier si l'animal a des images
+            const imageSlug = animal.imageSlug;
+
+            const animalCard = document.createElement("div");
+            animalCard.classList.add("col-sm-12", "col-md-4", "col-lg-3");
+
+            animalCard.innerHTML = `
+                                <div class="img-card">
+                                    <img class="w-100 rounded" src="${imageSlug}" alt="${animal.prenom}">
+                                    <div class="action-image-buttons" data-show="admin">
+                                        <button type="button" class="btn text-primary" data-bs-toggle="modal"
+                                            data-bs-target="#EditAnimalModal"><i class="bi bi-pencil-square"></i></button>
+                                        <button type="button" class="btn text-success" data-bs-toggle="modal"
+                                            data-bs-target="#RapportAnimalModal"><i class="bi bi-file-medical"></i></button>
+                                        <button type="button" class="btn text-danger" data-bs-toggle="modal"
+                                            data-bs-target="#SuppressionAnimalModal"><i class="bi bi-trash"></i></button>
+                                    </div>
+                                    <button id="info-animal" data-bs-toggle="modal" data-bs-target="#InfosAnimalModal"
+                                        class="btn btn-secondary info-animal">En savoir plus</button>
+                                </div>
+                            `;
+
+            animalsContainer.appendChild(animalCard);
+          });
+        })
+        .catch((error) => console.error("Error fetching animals:", error));
+    });
+  });
+
+
+  
+// Fetch pour un nouveau rapport vétérinaire
 const submitButton = document.querySelector("#RapportAnimalModal .btn-primary");
 
 if (submitButton) {
@@ -31,7 +100,7 @@ if (submitButton) {
     };
 
     // Envoyer les données avec Fetch API
-    fetch("/api/rapports/new", {
+    fetch("https://127.0.0.1:8000/api/rapports/new", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
